1ac5a9aee646c69e95b19dc3041c3f9a3957be9b
Nick <nplace.np@gmail.com>
2020-08-26 20:32:20 -0700

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,19 +1,14 @@
 from pathlib import Path
 
 class Site:
-    """
-    Class to generate configuration values 
-    and to create root structure of our static site.
-    """
+
 
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        """
-        Create Directory
-        """
+        
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exists_ok=True)
 
