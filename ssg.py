3cd509a3e6cb8eb7d87546d4eafa373d10cddbf0
Nick <nplace.np@gmail.com>
2020-09-04 20:51:50 -0700

    Completed module

--- ssg.py
+++ ssg.py
@@ -1,17 +1,20 @@
 import typer
-
 from ssg.site import Site
 
 import ssg.parsers
 
-def main(source='content', dest='dist'):
 
+def main(source="content", dest="dist"):
     config = {
-        'source': source,
-        'dest': dest,
-        'parsers': [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()],
+        "source": source,
+        "dest": dest,
+        "parsers": [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
     }
-
     Site(**config).build()
+
 
 typer.run(main)
